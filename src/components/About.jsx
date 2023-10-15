b4fc59b593c7dae4da440e8386f0f88bb3805a3f
Colin Huber <cbhuber@gmail.com>
2023-10-14 22:12:30 -0600

    feat: higer-order-component section wrapper for section x-padding.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,9 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
+import SectionWrapper from "../hoc/SectionWrapper";
 
+/* eslint react/prop-types: 0 */
 const ServiceCard = ({ index, title, icon }) => (
   <Tilt className="xs:w-[250px] w-full">
     <motion.div
@@ -60,4 +62,5 @@ const About = () => {
   );
 };
 
-export default About;
+// export default About;
+export default SectionWrapper(About, "about");
---
+++ src/hoc/SectionWrapper.jsx
@@ -0,0 +1,28 @@
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { staggerContainer } from "../utils/motion";
+
+// Higher Order Component that returns a function
+const SectionWrapper = (Component, idName) =>
+  function HOC() {
+    return (
+      // Add padding to sections on the portfolio page
+      <motion.section
+        variants={staggerContainer()}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true, amount: 0.25 }} // quarter of a second
+        className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
+      >
+        <span className="hash-span" id={idName}>
+          &nbsp;
+        </span>
+
+        {/* Render original component here */}
+        <Component />
+      </motion.section>
+    );
+  };
+
+export default SectionWrapper;
