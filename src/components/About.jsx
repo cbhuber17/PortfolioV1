2870eba25d866cb80f4c073bd03a2006a8a7250e
Colin Huber <cbhuber@gmail.com>
2023-10-15 14:21:09 -0600

    refactor: move ServiceCard to its own file.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,37 +1,10 @@
-import React from "react";
-import Tilt from "react-parallax-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
+import ServiceCard from "./ServiceCard";
 import SectionWrapper from "../hoc/SectionWrapper";
-
-/* eslint react/prop-types: 0 */
-const ServiceCard = ({ index, title, icon }) => (
-  <Tilt className="xs:w-[250px] w-full">
-    <motion.div
-      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
-      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
-    >
-      {/* Tilt options */}
-      <div
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
-      >
-        <img src={icon} alt={title} className="w-16 h-16 object-contain" />
-
-        <h3 className="text-white text-[20px] font-bold text-center">
-          {title}
-        </h3>
-      </div>
-    </motion.div>
-  </Tilt>
-);
 
 const About = () => {
   return (
