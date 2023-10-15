98ca4c8d29da1d7fd3b8bef5600db37bfd8af02a
Colin Huber <cbhuber@gmail.com>
2023-10-14 19:33:15 -0600

    fix: temp patch to ignore three-fiber unknown props.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,19 +2,22 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  settings: { react: { version: "18.2" } },
+  plugins: ["react-refresh"],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    // This is just a band-aid, not a fix
+    // See https://github.com/pmndrs/react-three-fiber/tree/master/packages/eslint-plugin#react-threeeslint-plugin
+    "react/no-unknown-property": ["off", { ignore: ["JSX"] }],
   },
-}
+};
