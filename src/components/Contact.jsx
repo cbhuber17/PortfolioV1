f30e2275c28112b3eac614bc2ab0dd594a29cd28
Colin Huber <cbhuber@gmail.com>
2023-10-18 18:07:52 -0400

    feat: transition animation on button hover.

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -117,7 +117,7 @@ const Contact = () => {
 
           <button
             type="submit"
-            className="bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary hover:bg-yellow-400 hover:text-black"
+            className="bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary hover:bg-yellow-400 hover:text-black transition-all"
           >
             {loading ? "Sending..." : "Send"}
           </button>
