522ca14d83da1fe7d54f31f718ba8ecc7f814ee4
Colin Huber <cbhuber@gmail.com>
2023-10-19 09:34:29 -0400

    feat: Add check on number techs.

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -2,10 +2,14 @@ import { BallCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 import { shuffle } from "../utils/shuffle";
+import toast from "react-hot-toast";
 
 const Tech = () => {
   shuffle(technologies);
-  // TODO: Check for maximum of 14 techs allowed
+  if (technologies.length > 13) {
+    toast.error("Too many WebGL images. Remove some WebGL images.");
+    return;
+  }
 
   return (
     <div className="flex flex-row flex-wrap justify-center gap-10">
