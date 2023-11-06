46a1225d5d481b9248579e792b9c05370cde6387
Colin Huber <cbhuber@gmail.com>
2023-11-06 13:05:02 -0700

    fix: remove motion from addendum cards.

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -12,67 +12,126 @@ const ProjectCard = ({
   image,
   source_code_link,
   project_link,
+  show_motion = true,
 }) => {
   return (
-    <motion.div variants={fadeIn("up", "spring", index * 0.25, 0.75)}>
-      <Tilt
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
-        className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full card"
-      >
-        <div className="relative w-full h-[230px]">
-          <img
-            src={image}
-            alt="project_image"
-            className="w-full h-full object-cover rounded-2xl"
-          />
-
-          <div className="absolute inset-0 flex justify-end space-x-1 m-3 card-img_hover">
-            <div
-              onClick={() => window.open(source_code_link, "_blank")}
-              className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
-            >
+    <>
+      {/* Motion lib limited to number of ProjectCard renders */}
+      {/* MOTION CARD */}
+      {show_motion ? (
+        <motion.div variants={fadeIn("up", "spring", index * 0.25, 0.75)}>
+          <Tilt
+            options={{
+              max: 45,
+              scale: 1,
+              speed: 450,
+            }}
+            className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full card"
+          >
+            <div className="relative w-full h-[230px]">
               <img
-                src={github}
-                alt="Source code"
-                className="w-1/2 h-1/2 object-contain"
+                src={image}
+                alt="project_image"
+                className="w-full h-full object-cover rounded-2xl"
               />
+
+              <div className="absolute inset-0 flex justify-end space-x-1 m-3 card-img_hover">
+                <div
+                  onClick={() => window.open(source_code_link, "_blank")}
+                  className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+                >
+                  <img
+                    src={github}
+                    alt="Source code"
+                    className="w-1/2 h-1/2 object-contain"
+                  />
+                </div>
+                {project_link && (
+                  <div
+                    onClick={() => window.open(project_link, "_blank")}
+                    className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+                  >
+                    <img
+                      src={external}
+                      alt="Project link"
+                      className="w-1/2 h-1/2 object-contain"
+                    />
+                  </div>
+                )}
+              </div>
             </div>
-            {project_link && (
+
+            <div className="mt-5">
+              <h3 className="text-white font-bold text-[24px]">{name}</h3>
+              <p className="mt-2 text-secondary text-[14px]">{description}</p>
+            </div>
+
+            <div className="mt-4 flex flex-wrap gap-2">
+              {tags.map((tag) => (
+                <p
+                  key={`${name}-${tag.name}`}
+                  className={`text-[14px] ${tag.color}`}
+                >
+                  #{tag.name}
+                </p>
+              ))}
+            </div>
+          </Tilt>
+        </motion.div>
+      ) : (
+        // NO MOTION CARD
+        <div className="bg-tertiary p-5 rounded-2xl sm:w-[360px] w-full card [box-shadow:8px_14px_38px_rgba(39,44,49,.06),_1px_3px_8px_rgba(39,44,49,.03)] [transition:all_.5s_ease] hover:[box-shadow:8px_28px_50px_rgba(39,44,49,.07),_1px_6px_12px_rgba(39,44,49,.04)] hover:[transition:all_.4s_ease] hover:[transform:translate3D(0,-1px,0)_scale(1.1)]">
+          <div className="relative w-full h-[230px]">
+            <img
+              src={image}
+              alt="project_image"
+              className="w-full h-full object-cover rounded-2xl"
+            />
+
+            <div className="absolute inset-0 flex justify-end space-x-1 m-3 card-img_hover">
               <div
-                onClick={() => window.open(project_link, "_blank")}
+                onClick={() => window.open(source_code_link, "_blank")}
                 className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
               >
                 <img
-                  src={external}
-                  alt="Project link"
+                  src={github}
+                  alt="Source code"
                   className="w-1/2 h-1/2 object-contain"
                 />
               </div>
-            )}
+              {project_link && (
+                <div
+                  onClick={() => window.open(project_link, "_blank")}
+                  className="black-gradient w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
+                >
+                  <img
+                    src={external}
+                    alt="Project link"
+                    className="w-1/2 h-1/2 object-contain"
+                  />
+                </div>
+              )}
+            </div>
+          </div>
+
+          <div className="mt-5">
+            <h3 className="text-white font-bold text-[24px]">{name}</h3>
+            <p className="mt-2 text-secondary text-[14px]">{description}</p>
+          </div>
+
+          <div className="mt-4 flex flex-wrap gap-2">
+            {tags.map((tag) => (
+              <p
+                key={`${name}-${tag.name}`}
+                className={`text-[14px] ${tag.color}`}
+              >
+                #{tag.name}
+              </p>
+            ))}
           </div>
         </div>
-
-        <div className="mt-5">
-          <h3 className="text-white font-bold text-[24px]">{name}</h3>
-          <p className="mt-2 text-secondary text-[14px]">{description}</p>
-        </div>
-
-        <div className="mt-4 flex flex-wrap gap-2">
-          {tags.map((tag) => (
-            <p
-              key={`${name}-${tag.name}`}
-              className={`text-[14px] ${tag.color}`}
-            >
-              #{tag.name}
-            </p>
-          ))}
-        </div>
-      </Tilt>
-    </motion.div>
+      )}
+    </>
   );
 };
 
--- src/pages/Addendum.jsx
+++ src/pages/Addendum.jsx
@@ -1,7 +1,5 @@
 import { Navbar } from "../components";
-import { motion } from "framer-motion";
 import ParagraphHeader from "./../components/ParagraphHeader";
-import { fadeIn, textVariant } from "../utils/motion";
 import { SectionWrapper } from "../hoc";
 import { projects } from "../constants";
 import ProjectCard from "./../components/ProjectCard";
@@ -12,39 +10,33 @@ const Addendum = () => {
       <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
         <Navbar />
       </div>
-      <motion.div variants={textVariant()}>
-        <ParagraphHeader pText="All Projects" hText="Addendum." style="" />
-      </motion.div>
+      <ParagraphHeader pText="All Projects" hText="Addendum." style="" />
 
-      <motion.p
-        variants={fadeIn("", "", 0.1, 1)}
-        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
-      >
+      <p className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
         All projects are shown below.
-      </motion.p>
+      </p>
 
       {/* TODO: Filter button: Sort by: Popular, alphabetical, created date asc/desc  */}
 
       <div className="card-grid mt-20 flex flex-wrap gap-7" id="certificates">
         {projects.map((project, index) => (
-          <ProjectCard key={`project-${index}`} index={index} {...project} />
+          <ProjectCard
+            key={`project-${index}`}
+            show_motion={false}
+            {...project}
+          />
         ))}
       </div>
 
-      <motion.div variants={textVariant()}>
-        <ParagraphHeader
-          pText="All Certificates"
-          hText="Extracurricular Learning."
-          style=""
-        />
-      </motion.div>
+      <ParagraphHeader
+        pText="All Certificates"
+        hText="Extracurricular Learning."
+        style=""
+      />
 
-      <motion.p
-        variants={fadeIn("", "", 0.1, 1)}
-        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
-      >
+      <p className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]">
         All certificates are shown below.
-      </motion.p>
+      </p>
     </div>
   );
 };
