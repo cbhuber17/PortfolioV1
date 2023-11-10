d8329f817299ca41200744331ca0ce8b4b40fe92
Colin Huber <cbhuber@gmail.com>
2023-11-09 20:44:52 -0700

    fix: remove empty line.

--- src/constants/index.js
+++ src/constants/index.js
@@ -1241,7 +1241,6 @@ const certificates = [
       "ISTQB® is the leading global certification program in the field of software testing.",
     link: "https://cstb.ca/certified-testers?certlevelid=1&search=huber",
   },
-
   {
     img: "apega.png",
     credential: "P. Eng, Professional Engineer",
