56ea585b80495664f29ec0906ac35f76aacde02c
Colin Huber <cbhuber@gmail.com>
2023-11-16 11:07:09 -0700

    feat: About translation (VN).

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,14 +6,21 @@ import ParagraphHeader from "./ParagraphHeader";
 import ServiceCard from "./ServiceCard";
 import SectionWrapper from "../hoc/SectionWrapper";
 import { shuffle } from "../utils/shuffle";
+import { useLanguage } from "../contexts/LanguageContext";
 
 // eslint-disable-next-line react-refresh/only-export-components
 const About = () => {
+  const { isForeign } = useLanguage();
   shuffle(services);
+
   return (
     <>
       <motion.div variants={textVariant()}>
-        <ParagraphHeader pText="Introduction" hText="Overview." style="" />
+        {isForeign ? (
+          <ParagraphHeader pText="Giới Thiệu" hText="Tổng Quan." style="" />
+        ) : (
+          <ParagraphHeader pText="Introduction" hText="Overview." style="" />
+        )}
       </motion.div>
 
       {/* Direction, type, delay(s), duration(s) */}
@@ -21,28 +28,17 @@ const About = () => {
         variants={fadeIn("", "", 0.1, 1)}
         className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        Welcome! I am a lead GNSS software tester with more than 15 years of
-        experience working with testing and developing GNSS products. I have
-        mastered the use of GNSS simulators to effectively test and improve the
-        quality of products.
+        <p>
+          {isForeign
+            ? "Chào mừng bạn đến với danh mục đầu tư của tôi, nơi sự đổi mới đáp ứng được độ chính xác trong lĩnh vực kỹ thuật số. Tôi là một Nhà phát triển web Full Stack có kinh nghiệm với niềm đam mê tạo ra các giải pháp liền mạch và có thể mở rộng để kết nối khoảng cách giữa trải nghiệm người dùng và chức năng mạnh mẽ. tôi đã sẵn sàng để cung cấp các giải pháp web chất lượng cao, toàn diện."
+            : "Welcome to my portfolio, where innovation meets precision in the digital realm. I am an experienced Full Stack Web Developer with a passion for crafting seamless and scalable solutions that bridge the gap between user experience and robust functionality. I am dedicated to delivering high-quality, end-to-end web solutions."}
+        </p>
         <br />
-        <br /> I value being organized and implementing 5S/6S office routines. I
-        also look for methods of continuous improvement, innovation, and
-        advancing my own education, which is the foundation of being an
-        engineer.
-        <br />
-        <br /> I choose to set quality standards to “very high” to meet
-        customer’s expectations and deliverables and this is what drives
-        success. I am not afraid to speak up to call out violations in processes
-        and threats to quality! Finding those software bugs is what excites me
-        the most about being a software tester.
-        <br />
-        <br /> I have founded a new passion with full stack web development and
-        cloud solutions via AWS. Bringing the complex nature of the web to
-        people’s devices using modern techniques is incredibly exciting and
-        challenging. There is unlimited potential as to what can be brought to
-        the world with full stack skills, perhaps one day having GNSS on the
-        cloud! The learning never stops, so let’s keep it going!
+        <p>
+          {isForeign
+            ? "Tôi cũng xuất sắc trong các công nghệ GNSS trong đó độ chính xác và độ chính xác là điều tối quan trọng. Tôi có con mắt tinh tường về chi tiết, đảm bảo rằng hệ thống định vị đáp ứng các tiêu chuẩn chất lượng nghiêm ngặt cho các ứng dụng quan trọng về an toàn. Cách tiếp cận tỉ mỉ này áp dụng cho công việc của tôi trong Giải pháp đám mây, đặc biệt là với AWS. Tận dụng sức mạnh của đám mây, tôi thiết kế và triển khai các kiến ​​trúc an toàn và có thể mở rộng nhằm nâng cao hiệu suất và khả năng phục hồi của các ứng dụng nhằm đáp ứng nhu cầu của công nghệ hiện đại."
+            : "I also excel in GNSS technologies where precision and accuracy are paramount. I have a keen eye for detail, ensuring that navigation systems meet stringent quality standards for safety-critical applications. This meticulous approach extends to my work in Cloud Solutions, particularly with AWS. Leveraging the power of the cloud, I design and implement scalable and secure architectures that enhance the performance and resilience of applications to meet the demands of modern technology."}
+        </p>
       </motion.p>
       <div className="mt-20 flex flex-wrap gap-10">
         {services.map((service, index) => (
