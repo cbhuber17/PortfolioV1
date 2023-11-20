ce0e74b6b66c85243c88d35bf329d9fba24df3c3
Colin Huber <cbhuber@gmail.com>
2023-11-20 14:20:38 -0700

    feat: certificates section responsive behavior changes to framer motion.

--- src/components/Certificates.jsx
+++ src/components/Certificates.jsx
@@ -7,30 +7,56 @@ import ParagraphHeader from "./ParagraphHeader";
 import certificateStyles from "./Certificates.module.css";
 import { certificates } from "../constants";
 import { useLanguage } from "../contexts/LanguageContext";
+import useWindowDimensions from "../hooks/useWindowDimensions";
 
 // eslint-disable-next-line react-refresh/only-export-components
 const Certificates = () => {
   const { isForeign } = useLanguage();
+  const { width } = useWindowDimensions();
+  let isMobile = false;
+
+  if (width < 500) isMobile = true;
 
   return (
     <>
-      <motion.div variants={textVariant()}>
-        {isForeign ? (
-          <ParagraphHeader pText="Giáo dục" hText="Chứng chỉ" style="" />
-        ) : (
-          <ParagraphHeader pText="Education" hText="Certificates" style="" />
-        )}
-      </motion.div>
+      {isMobile ? (
+        <div variants={textVariant()}>
+          {isForeign ? (
+            <ParagraphHeader pText="Giáo dục" hText="Chứng chỉ" style="" />
+          ) : (
+            <ParagraphHeader pText="Education" hText="Certificates" style="" />
+          )}
+        </div>
+      ) : (
+        <motion.div variants={textVariant()}>
+          {isForeign ? (
+            <ParagraphHeader pText="Giáo dục" hText="Chứng chỉ" style="" />
+          ) : (
+            <ParagraphHeader pText="Education" hText="Certificates" style="" />
+          )}
+        </motion.div>
+      )}
 
       <div className="w-full flex mb-10">
-        <motion.p
-          variants={fadeIn("", "", 0.1, 1)}
-          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
-        >
-          {isForeign
-            ? "Dưới đây bạn sẽ tìm thấy thông tin xác thực và chứng chỉ của tôi trong những năm qua."
-            : "Below you will find my credentials and certificates over the years."}
-        </motion.p>
+        {isMobile ? (
+          <p
+            variants={fadeIn("", "", 0.1, 1)}
+            className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+          >
+            {isForeign
+              ? "Dưới đây bạn sẽ tìm thấy thông tin xác thực và chứng chỉ của tôi trong những năm qua."
+              : "Below you will find my credentials and certificates over the years."}
+          </p>
+        ) : (
+          <motion.p
+            variants={fadeIn("", "", 0.1, 1)}
+            className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+          >
+            {isForeign
+              ? "Dưới đây bạn sẽ tìm thấy thông tin xác thực và chứng chỉ của tôi trong những năm qua."
+              : "Below you will find my credentials and certificates over the years."}
+          </motion.p>
+        )}
       </div>
 
       <div className={certificateStyles.container}>
