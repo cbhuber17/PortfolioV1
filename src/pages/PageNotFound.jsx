117a6547b26863b00831e42a83f281582ee37ec7
Colin Huber <cbhuber@gmail.com>
2023-11-19 18:59:00 -0700

    feat: animate not found page.

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,12 +1,37 @@
 import { Navbar } from "../components";
+import { useLanguage } from "../contexts/LanguageContext";
+// import "./PageNotFound.module.css";
 
 export default function PageNotFound() {
+  const { isForeign } = useLanguage();
+
   return (
     <div className="relative z-0 bg-primary h-screen">
       <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
         <Navbar />
       </div>
-      <h1 className="mt-[70px] text-center text-xl">Page Not Found!</h1>
+      <h1 className="z-10 mt-[70px] text-center text-xl">
+        {isForeign ? "Không Tìm Thấy Trang!" : "Page Not Found!"}
+      </h1>
+
+      <section className="noisecontainer">
+        <svg className="svgcontainer">
+          <filter id="noise">
+            <feTurbulence id="turbulence">
+              <animate
+                attributeName="baseFrequency"
+                dur="50s"
+                values="0.9 0.9;0.8 0.8,0.9 0.9"
+                repeatCount="indefinite"
+              ></animate>
+            </feTurbulence>
+            <feDisplacementMap
+              in="SourceGraphic"
+              scale="60"
+            ></feDisplacementMap>
+          </filter>
+        </svg>
+      </section>
     </div>
   );
 }
