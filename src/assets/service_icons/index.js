3aef19efdd5a5106c9e773a7366608a97744f3f4
Colin Huber <cbhuber@gmail.com>
2024-10-06 14:13:52 -0600

    feat: Updated service icons

--- src/assets/service_icons/index.js
+++ src/assets/service_icons/index.js
@@ -1,6 +1,7 @@
 import full_stack from "./full_stack.png";
 import gnss from "./gnss.png";
 import cloud from "./cloud.png";
-import tutor from "./tutor.png";
+// import tutor from "./tutor.png";
+import mentor from "./mentoring.png";
 
-export { full_stack, gnss, cloud, tutor };
+export { full_stack, gnss, cloud, mentor };
