480ed797730172f53fec6573a77924e68c6eecc5
Colin Huber <cbhuber@gmail.com>
2026-02-19 20:55:20 -0700

    fix: Scroll Icon link

--- src/components/ScrollIcon.jsx
+++ src/components/ScrollIcon.jsx
@@ -3,7 +3,15 @@ import { motion } from "framer-motion";
 const ScrollIcon = () => {
   return (
     <div className="absolute bottom-2 w-full flex justify-center items-center">
-      <a href="#about">
+      <a
+        href="#"
+        onClick={(e) => {
+          e.preventDefault();
+          document
+            .getElementById("about")
+            ?.scrollIntoView({ behavior: "smooth" });
+        }}
+      >
         <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
           <motion.div
             animate={{
